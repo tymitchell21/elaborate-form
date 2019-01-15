d73ca5d7e7b1c2a1a452a8bc98bb0c88c32ad93c
Tyler <tymitchell2100@gmail.com>
2019-01-15 16:04:05 -0500

    final commit

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
 const express = require("express")
 const path = require("path")
+const cors = require('cors')
 
 const app = express()
 const publicFolderPath = path.join(__dirname, "public")
 
 app.use(express.json())
 app.use(express.static(publicFolderPath))
+app.use(cors())
 
-const users = []
+let users = {}
+let userID = 1
 
 // add POST request listener here
+app.post('/api/user/', function (req, res) {
 
-app.listen(3000);
+    const responseBody = req.body
+    console.log(responseBody.username)
+
+    if (!users[responseBody.username]) {
+        users[responseBody.username] = responseBody
+        users[responseBody.username].id = userID
+        userID++
+        res.statusCode = 201
+
+        res.send(req.body)
+        res.end()
+    } else {
+        res.statusCode = 409
+        res.end("USERNAME OR PASSWORD ALREADY TAKEN")
+    }
+})
+
+app.get('/api/user/', function (req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/javascript');
+    const responseBody = JSON.stringify(users);
+    res.send(responseBody);
+    res.end(responseBody)
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
--- public/index.js
+++ public/index.js
@@ -1,2 +1,42 @@
 const userCreateForm = document.getElementById("user-create-form")
 const userCreateSubmitButton = userCreateForm.querySelector("button[type='submit']")
+const responseH1 = document.querySelector('#response')
+
+const url = 'http://127.0.0.1:3000/api/user/'
+
+userCreateForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    const email = document.getElementById('email').value
+    const username = document.getElementById('username').value
+    const firstName = document.getElementById('first-name').value
+    const lastName = document.getElementById('last-name').value
+    const age = document.getElementById('age').value
+
+    const user = {
+        username: username,
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+    }
+
+    fetch(url, {
+        method: 'POST',
+        node: 'no-cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    .then(response => {
+        if (response.status == 409) {
+            responseH1.innerHTML = 'Username is already taken'
+        } else {
+            responseH1.innerHTML = 'Username has been added'
+        }
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
